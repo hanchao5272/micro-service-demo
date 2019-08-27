a3cd99623ade5b19f3dd2042899135ad87308d34
hanchao <hanchao@yidian-inc.com>
2019-08-27 18:09:08 +0800

    add:单节点service-order

---
+++ service-order/src/main/java/pers/hanchao/microservicedemo/serviceorder/ServiceOrderApplication.java
@@ -0,0 +1,18 @@
+package pers.hanchao.microservicedemo.serviceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author hanchao
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrderApplication.class, args);
+    }
+
+}
