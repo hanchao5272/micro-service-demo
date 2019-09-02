00431017f26ebdea778deed3a0e3bae01262c6a1
hanchao <hanchao@yidian-inc.com>
2019-09-02 11:12:56 +0800

    add:hystrix dashboard

--- service-order/src/main/java/pers/hanchao/microservicedemo/serviceorder/ServiceOrderApplication.java
+++ service-order/src/main/java/pers/hanchao/microservicedemo/serviceorder/ServiceOrderApplication.java
@@ -1,13 +1,23 @@
 package pers.hanchao.microservicedemo.serviceorder;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.HystrixStreamEndpoint;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 /**
  * @author hanchao
  */
+@EnableHystrixDashboard
+@EnableHystrix
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
@@ -17,4 +27,19 @@ public class ServiceOrderApplication {
         SpringApplication.run(ServiceOrderApplication.class, args);
     }
 
+    /**
+     * https://www.cnblogs.com/x1mercy/p/9291348.html
+     * 流式： http://127.0.0.1:6061/hystrix.stream
+     * 图形： http://127.0.0.1:6061/hystrix  (参数：http://127.0.0.1:6061/hystrix.stream) (刷新流：http://127.0.0.1:6061/order/1)
+     * @see HystrixMetricsStreamServlet
+     * @see HystrixStreamEndpoint
+     */
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(new HystrixMetricsStreamServlet());
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName(HystrixMetricsStreamServlet.class.getSimpleName());
+        return registrationBean;
+    }
 }
